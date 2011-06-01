46722e8b406687dd2596934afacba2b64659677c
kaz6120 <kaz6120@gmail.com>
2011-06-02 05:50:24 +0900

    Added Japanese language reasource to "Usermap" plugin."

---
+++ usermap/lang/Japanese.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Copyright (C) 2010 Justgizzmo.com
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ */
+
+$lang_usermap = array(
+	'User map'					=> 'ユーザーマップ',
+	'User map legend'			=> 'ユーザーマップオプション',
+	'User list'					=> 'ユーザーリスト',
+	'No users'					=> '現在ユーザーの登録はありません。',
+	'Find location'				=> 'ロケーションを探す',
+	'Find location help'		=> 'If you can not find your location and would like some help to do so, we can do so by your ip address. You can then move the marker to a more precise location. This is not guaranteed to work 100% of the time.',
+	'User map help'				=> '<strong>Help:</strong> To set your location, click and move the map to your location. Click on the map to add a marker. You can drag the marker around to get a more precise location. To remove your location, click on the marker.',
+	'Scrollwheel zoom help'		=> 'マウススクロールでズームアップする。',
+	'Save as default location'	=> 'デフォルトロケーションとして保存',
+);
---
+++ usermap/lang/Japanese_admin.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Copyright (C) 2010 Justgizzmo.com
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ */
+
+$lang_usermap_admin = array(
+	'User map settings'			=> 'User map settings',
+
+	// group options and permissions
+	'Group updated redirect'	=> 'Group updated. Redirecting …',
+
+	'Cur group'					=> '%s options and permissions',
+	'User map icon'				=> 'User map icon',
+	'User map icon info'		=> 'The icon to represent users in this group on the user map.',
+	'User map viewing'			=> 'View user map',
+	'User map viewing info'		=> 'Allow users in this group to view the user map.',
+	'User map add to'			=> 'Add to user map',
+	'User map add to info'		=> 'Allow users in this group to add their location to the user map.',
+
+	// global options
+	'Options updated redirect'	=> 'Options updated. Redirecting …',
+
+	'Options'					=> 'Options',
+	'Options info'				=> 'To set the default location, view the <a href="'.$pun_config['o_base_url'].'/usermap.php">main map</a>, set the map to the location you want as the default, then right click on the map and click \'Save as default location\'. You will be sent back here with the info already entered. Then just click Save.',
+	'Default latitude'			=> 'Default latitude',
+	'Default longitude'			=> 'Default longitude',
+	'Default zoom'				=> 'Default zoom',
+	'Default zoom info'			=> 'Zoom level 0 is the farthest and 19 is the closest.',
+	'Map height'				=> 'Map height',
+	'Map height info'			=> 'The height of the map in pixels (between 300 and 1,000). The default is 500.',
+	'Fit map zoom'				=> 'Fit map zoom',
+	'Fit map zoom info'			=> 'The map will automatically set its location and zoom to make sure every marker is shown. This will override the above settings.',
+	'Save options'				=> 'Save options',
+
+	// pre group options
+	'Groups'					=> 'Groups',
+	'Groups info'				=> 'Please note that this may look like the normal group options, it\'s not. You can not delete or change any options other than options related to the user map mod.',
+	'Edit'						=> 'Edit',
+);
